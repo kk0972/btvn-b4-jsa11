0722d4e96e663c2d297ecc0caf43788d5434f132
kk0972 <72807187+kk0972@users.noreply.github.com>
2022-07-31 09:56:19 +0700

    Delete 2.js

--- 2.js
+++
@@ -1,7 +0,0 @@
-var text = document.getElementById("text");
-text.addEventListener('mouseover', function(){
-    text.style.color = "blue"
-})
-text.addEventListener('mouseleave', function(){
-    text.style.color = "red"
-})
